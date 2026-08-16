616bb3bda115f65acc150a55bdc849a2090b221c
Sasamuelll <148778945+Sasamuelll@users.noreply.github.com>
2026-08-15 20:08:52 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // поменяйте на адрес вашего сайта (нужно для RSS)
-  site: 'https://example.com',
+  site: 'https://sasamuelll.github.io',
   markdown: {
     // подсветка кода отключена: весь код рисуется одним «фосфорным» цветом,
     // как на настоящем терминале
